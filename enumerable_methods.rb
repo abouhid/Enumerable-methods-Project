7bf231af49c61467be0f4873068e0aba0608ff83
abouhid <alexandre.bouhid@engenharia.ufjf.br>
2020-07-29 21:24:42 -0300

    Edit rubocop.yml for linters

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -2,16 +2,19 @@ module Enumerable
   def my_each
     return to_enum unless block_given?
 
-    size.times { |i| yield(to_a[i]) }
-    self
+    for x in self
+      yield(x)
+    end
   end
 
   def my_each_with_index
     return to_enum unless block_given?
 
     i = 0
-    size.times { |i| yield(to_a[i], i) }
-    self
+    for x in self
+      yield(x, i)
+      i += 1
+    end
   end
 
   def my_select
@@ -45,9 +48,7 @@ module Enumerable
       end
     elsif arg[0].nil?
       my_each do |el|
-        if el != false && el != nil
-          i += 1
-        end
+        i += 1 if el != false && !el.nil?
       end
     else
       my_each do |el|
@@ -78,9 +79,7 @@ module Enumerable
       end
     elsif arg[0].nil?
       my_each do |el|
-        if el != false && el != nil
-          i += 1
-        end
+        i += 1 if el != false && !el.nil?
       end
     else
       my_each do |el|
@@ -112,9 +111,7 @@ module Enumerable
       end
     elsif arg[0].nil?
       my_each do |el|
-        if el == false || el == nil
-          i += 1
-        end
+        i += 1 if el == false || el.nil?
       end
     else
       my_each do |el|
@@ -156,21 +153,21 @@ module Enumerable
   end
 
   def my_inject(num = nil, sym = nil)
-    arr = self.to_a
+    arr = to_a
 
     if block_given?
-      if num != nil
+      if !num.nil?
         my_each do |el|
           num = yield(num, el)
         end
-        num
+
       else
         num = arr[0]
         arr[1..-1].my_each do |el|
           num = yield(num, el)
         end
-        num
       end
+      num
     elsif !block_given?
       if num.class == Symbol || num.class == String
         sum = nil
@@ -193,75 +190,75 @@ module Enumerable
   end
 end
 
-p "My_each method:"
-p([nil, 2.5, "hi", -7, true].my_each { |el| el })
+p 'My_each method:'
+p([nil, 2.5, 'hi', -7, true].my_each { |el| el })
 puts
-p "My_each_with_index:"
-[nil, 2.5, "hi", -7, true].my_each_with_index { |el, i| p("Element: #{el}, Index: #{i}") }
+p 'My_each_with_index:'
+[nil, 2.5, 'hi', -7, true].my_each_with_index { |el, i| p("Element: #{el}, Index: #{i}") }
 puts
-p "My_select method:"
+p 'My_select method:'
 [1, 2, 3, 4, 5, 6].my_select { |n| n > 3 }
 puts
-p "My_all? method:"
-p "Using block:"
+p 'My_all? method:'
+p 'Using block:'
 p(%w[ant ant ant].my_all? { |word| word.length >= 2 })
-p "Using Class:"
+p 'Using Class:'
 p(%w[ant ant ant].my_all?(String))
-p "Testing cases:"
-p(%w[ant ant ant].my_all?("ant"))
-p(%w[ant ant bear].my_all?("ant"))
-p(%w[bear cat dog].my_all?("ants"))
+p 'Testing cases:'
+p(%w[ant ant ant].my_all?('ant'))
+p(%w[ant ant bear].my_all?('ant'))
+p(%w[bear cat dog].my_all?('ants'))
 p([3, 5, 2].my_all?(Integer))
 puts
-p "My_any? method:"
-p "Using block:"
+p 'My_any? method:'
+p 'Using block:'
 p(%w[ant ant ant].my_any? { |word| word.length >= 2 })
-p "Using Class:"
+p 'Using Class:'
 p(%w[ant ant ant].my_any?(String))
-p "Testing cases:"
-p(%w[ant ant ant].my_any?("ant"))
-p(%w[ant ant bear].my_any?("ant"))
-p(%w[bear cat dog].my_any?("ants"))
+p 'Testing cases:'
+p(%w[ant ant ant].my_any?('ant'))
+p(%w[ant ant bear].my_any?('ant'))
+p(%w[bear cat dog].my_any?('ants'))
 
 puts
-p "My_none? method:"
-p "Using block:"
+p 'My_none? method:'
+p 'Using block:'
 p(%w[ant ant ant].my_none? { |word| word.length >= 2 })
-p "Using Class:"
+p 'Using Class:'
 p(%w[ant ant ant].my_none?(Integer))
-p "Testing cases:"
-p(%w[ant ant ant].my_none?("ant"))
-p(%w[ant ant bear].my_none?("ant"))
-p(%w[bear cat dog].my_none?("ants"))
+p 'Testing cases:'
+p(%w[ant ant ant].my_none?('ant'))
+p(%w[ant ant bear].my_none?('ant'))
+p(%w[bear cat dog].my_none?('ants'))
 p([3, 5, 2].my_none?(Integer))
 puts
-p "My_count method:"
-p "Using argument:"
+p 'My_count method:'
+p 'Using argument:'
 p([1, 2, 4, 23, 34, 143, 143, 143].my_count(143))
-p "Using block:"
+p 'Using block:'
 p([1, 2, 4, 23, 34, 143, 143, 143].my_count { |n| n > 20 })
-p "Without argument nor block:"
+p 'Without argument nor block:'
 p([1, 2, 4, 23, 34, 143, 143, 143].my_count)
 puts
-p "My_map method:"
-p "Using block:"
+p 'My_map method:'
+p 'Using block:'
 p([1, 2, 3, 22, 5].my_map { |x| x * 5 })
-p "Using proc:"
-proc1 = proc { |x| x ** 2 }
+p 'Using proc:'
+proc1 = proc { |x| x**2 }
 p [1, 2, 3, 22, 5].my_map(&proc1)
 puts
-p "My_inject method:"
-p "Using only argument:"
+p 'My_inject method:'
+p 'Using only argument:'
 p [1, 2, 3, 2, 5].my_inject(5)
-p "Using block:"
-p "Using argument and block:"
+p 'Using block:'
+p 'Using argument and block:'
 p([1, 2, 3, 2, 5].my_inject(5) { |sum, el| sum + el })
-p "Using string:"
-p [1, 2, 3, 2, 5].my_inject("+")
-p "Using symbol:"
+p 'Using string:'
+p [1, 2, 3, 2, 5].my_inject('+')
+p 'Using symbol:'
 p [1, 2, 3, 2, 5].my_inject(:-)
-p "Using argument and symbol:"
-p [1, 2, 3, 2, 5].my_inject(5,:-)
+p 'Using argument and symbol:'
+p [1, 2, 3, 2, 5].my_inject(5, :-)
 puts
-p "Multiply_els method:"
+p 'Multiply_els method:'
 p [].multiply_els([2, 4, 5])
